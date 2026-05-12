88cd088e96fb564ccc997b9c5cad8642f9140421
crawshawsammy-commits <crawshawsammy@gmail.com>
2026-05-12 12:26:37 +0100

    Add calculateScore function for scoring logic

--- lib/scoring.ts
+++ lib/scoring.ts
@@ -1,1 +1,17 @@
+export function calculateScore(
+  basePoints: number,
+  captain: boolean,
+  firstOfSeason: boolean
+) {
+  let total = basePoints
 
+  if (captain) {
+    total *= 2
+  }
+
+  if (firstOfSeason) {
+    total *= 2
+  }
+
+  return total
+}
