2558531e1d41d0e0b7dacc615e39ae4297b42fb9
crawshawsammy-commits <crawshawsammy@gmail.com>
2026-05-12 14:02:14 +0100

    Update instructions for selecting birds in MySquad

--- app/my-squad/page.tsx
+++ app/my-squad/page.tsx
@@ -1,11 +1,17 @@
+import birds from "../../data/birds.json"
+
 export default function MySquad() {
   return (
     <main style={{ padding: 40 }}>
       <h1>My Squad</h1>
 
-      <p>
-        Select 11 birds and choose a captain.
-      </p>
+      <p>Select 11 birds (UI coming next version)</p>
+
+      <ul>
+        {birds.map((b: any) => (
+          <li key={b.id}>{b.name}</li>
+        ))}
+      </ul>
     </main>
   )
 }
