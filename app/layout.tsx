eb51a4df643b6fbea11febd5b6fb2664930ed394
crawshawsammy-commits <crawshawsammy@gmail.com>
2026-05-12 13:38:44 +0100

    Add metadata and root layout for app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+export const metadata = {
+  title: "Fantasy Birding",
+  description: "Fantasy football, but for bird watching"
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  )
+}
